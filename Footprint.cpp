6fa80bee1c5fc4a532c07d45fd55aa100f43f0e6
kush9254 <kush.sharma2407@gmail.com>
2023-01-28 17:03:53 +0530

    var-name

--- Footprint.cpp
+++ Footprint.cpp
@@ -1,48 +1,31 @@
 #include "llvm/Pass.h"
-#include "llvm/IR/Module.h"
 #include "llvm/IR/Function.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/DebugInfoMetadata.h"
 #include "llvm/IR/Instruction.h"
-#include "llvm/IR/Instructions.h"
+#include "llvm/IR/DebugInfoMetadata.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/IntrinsicInst.h"
 
 using namespace llvm;
 
 namespace {
-    struct Footprint : public FunctionPass {
-        static char ID;
-        Footprint() : FunctionPass(ID) {}
+  struct Footprint : public FunctionPass {
+    static char ID;
+    Footprint() : FunctionPass(ID) {}
 
-        bool runOnFunction(Function &F) override {
-          /*
-            errs() << "Clang version: " << CLANG_VERSION << "\n";
-            errs() << "LLVM Source Repository: " << LLVM_REPOSITORY << "\n";
-            errs() << "LLVM Commit Hash: " << LLVM_COMMIT_HASH << "\n";
-            errs() << "Target: " << F.getParent()->getTargetTriple() << "\n";
-          */
-            for (auto &B : F) {
-                for (auto &I : B) {
-                    if (auto *op = dyn_cast<Instruction>(&I)) {
-                        for (unsigned i = 0, e = op->getNumOperands(); i != e; ++i) {
-                            Value *v = op->getOperand(i);
-                            if (isa<GlobalVariable>(v)) {
-                                errs() << "Variable Name: " << v->getName() << "\n";
-                                errs() << "1\n";
-                                errs() << "Variable Scope: " << op->getDebugLoc().getLine() << "\n";
-                                errs() << "Footprint: " << op->getDebugLoc().getCol() << "\n";
-                                errs() << "Number of Reads: " << v->getNumUses() << "\n";
-                                errs() << "Number of Writes: " << v->getNumUses() << "\n";
-                            }
-                        }
-                    }
-                }
-            }
-            return false;
+    bool runOnFunction(Function &F) override {
+      for (auto &BB : F) {
+        for (auto &I : BB) {
+          if (auto *DbgDecl = dyn_cast<DbgVariableIntrinsic>(&I)) {
+            auto *Var = DbgDecl->getVariable();
+            StringRef Variable = Var->getName();
+            errs() << "Variable name: " << Variable << "\n";
+          }
         }
-    };
+      }
+      return false;
+    }
+  };
 }
 
 char Footprint::ID = 0;
-static RegisterPass<Footprint> X("ftprint", "Counts the number of occurrences of a variable");
+static RegisterPass<Footprint> X("ftprint", "Variable Name Pass", false, false);
