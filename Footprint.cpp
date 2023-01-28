969ab3ae8618cecf0ee04e52123fbdecf9dc8a0c
kush9254 <kush.sharma2407@gmail.com>
2023-01-28 13:09:54 +0530

    28/06

--- Footprint.cpp
+++ Footprint.cpp
@@ -1,50 +1,48 @@
 #include "llvm/Pass.h"
+#include "llvm/IR/Module.h"
 #include "llvm/IR/Function.h"
-#include "llvm/IR/InstIterator.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/DebugInfoMetadata.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
 
 using namespace llvm;
 
 namespace {
-  struct Foo : public FunctionPass {
-    static char ID;
-    Foo() : FunctionPass(ID) {}
+    struct Footprint : public FunctionPass {
+        static char ID;
+        Footprint() : FunctionPass(ID) {}
 
-    bool runOnFunction(Function &F) override {
-      for (auto &BB : F) {
-        for (auto &I : BB) {
-          if (auto *op = dyn_cast<Instruction>(&I)) {
-             //errs()<<"1st if passed";
-            for (unsigned int i = 0, e = op->getNumOperands(); i != e; ++i) {
-             // errs()<<"inside for loop 1st\n";
-              Value *v = op->getOperand(i);
-              /*
-              if(isa<Instruction>(v))
-              {
-                errs()<<"inst\n";
-              }
-              else if(isa<Constant>(v))
-              errs()<<"const\n";
-              else if ( isa<Argument>(v) || isa<GlobalValue>(v))
-              errs()<<"something\n";
-              */
-              if(isa<Constant,GlobalValue,Argument,Instruction>(v)){
-             // if (auto *var = dyn_cast<Argument>(v)) {
-                //errs()<<"2nd if passed";
-                if (DebugLoc loc = op->getDebugLoc()) {
-                 // errs()<<"2nd if";
-                  errs() << "Variable '" << v->getName() << "' used at line " << loc->getLine() << "\n";
+        bool runOnFunction(Function &F) override {
+          /*
+            errs() << "Clang version: " << CLANG_VERSION << "\n";
+            errs() << "LLVM Source Repository: " << LLVM_REPOSITORY << "\n";
+            errs() << "LLVM Commit Hash: " << LLVM_COMMIT_HASH << "\n";
+            errs() << "Target: " << F.getParent()->getTargetTriple() << "\n";
+          */
+            for (auto &B : F) {
+                for (auto &I : B) {
+                    if (auto *op = dyn_cast<Instruction>(&I)) {
+                        for (unsigned i = 0, e = op->getNumOperands(); i != e; ++i) {
+                            Value *v = op->getOperand(i);
+                            if (isa<GlobalVariable>(v)) {
+                                errs() << "Variable Name: " << v->getName() << "\n";
+                                errs() << "1\n";
+                                errs() << "Variable Scope: " << op->getDebugLoc().getLine() << "\n";
+                                errs() << "Footprint: " << op->getDebugLoc().getCol() << "\n";
+                                errs() << "Number of Reads: " << v->getNumUses() << "\n";
+                                errs() << "Number of Writes: " << v->getNumUses() << "\n";
+                            }
+                        }
+                    }
                 }
-              }
             }
-          }
+            return false;
         }
-      }
-      return false;
-    }
-  };
+    };
 }
 
-char Foo::ID = 0;
-static RegisterPass<Foo> X("ftprint", "Prints the line number where a variable has been used");
+char Footprint::ID = 0;
+static RegisterPass<Footprint> X("ftprint", "Counts the number of occurrences of a variable");
