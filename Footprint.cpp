e68827d3828b3b38fdef10a3852f6f75a58b6736
kush9254 <kush.sharma2407@gmail.com>
2023-01-28 17:55:29 +0530

    name and line

--- Footprint.cpp
+++ Footprint.cpp
@@ -1,9 +1,12 @@
-#include "llvm/Pass.h"
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Instruction.h"
 #include "llvm/IR/DebugInfoMetadata.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/DebugLoc.h"
+
+#include <map>
 
 using namespace llvm;
 
@@ -12,20 +15,25 @@ namespace {
     static char ID;
     Footprint() : FunctionPass(ID) {}
 
-    bool runOnFunction(Function &F) override {
-      for (auto &BB : F) {
-        for (auto &I : BB) {
-          if (auto *DbgDecl = dyn_cast<DbgVariableIntrinsic>(&I)) {
-            auto *Var = DbgDecl->getVariable();
-            StringRef Variable = Var->getName();
-            errs() << "Variable name: " << Variable << "\n";
-          }
+   bool runOnFunction(Function &F) override {
+  for (auto &BB : F) {
+    for (auto &I : BB) {
+      if (auto *DbgDecl = dyn_cast<DbgVariableIntrinsic>(&I)) {
+        auto *Var = DbgDecl->getVariable();
+        StringRef Variable = Var->getName();
+        auto debugLoc = DbgDecl->getDebugLoc();
+        if ((I.getDebugLoc())) {
+          unsigned Line = debugLoc.getLine();
+          errs() << "Variable name: " << Variable << " used on line: " << Line << "\n";
         }
       }
-      return false;
     }
+  }
+  return false;
+}
+
   };
 }
 
 char Footprint::ID = 0;
-static RegisterPass<Footprint> X("ftprint", "Variable Name Pass", false, false);
+static RegisterPass<Footprint> X("ftprint", "Pass to count the usage of variables", false, false);
